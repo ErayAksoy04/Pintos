7a55c8686e34867de5688cb8149132273a99a3ab
ErayAksoy04 <eray.aksoy@ogr.gsu.edu.tr>
2026-03-25 02:28:51 +0300

    lock_release with priority restore

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -74,8 +74,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_insert_ordered (&sema->waiters, &thread_current ()->elem,
-                           compare_threads_by_priority, NULL);
+      list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
